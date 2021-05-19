f1aac329e791dd9b2f21f4b1536064f63d1898da
Ramiro Gómez <gomezramiro2002@gmail.com>
2021-05-19 12:36:19 -0300

    Added loader animation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
+/* eslint-disable no-console */
+import React, { useEffect } from 'react';
 import './App.scss';
 import Navbar from './components/Navbar';
 import Home from './components/sections/Home';
 import Projects from './components/sections/Projects';
 import About from './components/sections/About';
 import Contact from './components/sections/Contact';
-import Loader from './components/Loader';
 
 function App() {
+	const handleLoad = () => document.querySelector('.loader').classList.add('hidden');
+
+	useEffect(() => {
+		window.addEventListener('load', handleLoad);
+		return () => window.removeEventListener('load', handleLoad);
+	}, []);
+
 	return (
 		<div className="App">
-			<Loader />
 			<Navbar />
 			<Home />
 			<Projects />
